436cf2345b8f3dc59a83a834e0ef5b089609040a
Shodiyor Ergashev <shodiyorergashev970@gmail.com>
2024-10-10 17:45:59 +0500

    slider function created

---
+++ js/fv.js
@@ -0,0 +1,44 @@
+const swiper = new Swiper(".mySwiper", {
+  loop: true,
+  spaceBetween: 30,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  on: {
+    init: function () {
+      updateSlideClasses(this);
+    },
+    slideChange: function () {
+      updateSlideClasses(this);
+    },
+  },
+});
+
+function updateSlideClasses(swiper) {
+  swiper.slides.forEach((slide) => {
+    slide.classList.remove("main-slide", "side-slide");
+  });
+
+  const activeIndex = swiper.realIndex;
+  const slides = swiper.slides;
+
+  // Set the main slide class
+  slides[activeIndex].classList.add("main-slide");
+
+  // Set the side slides classes (previous and next)
+  const prevIndex = (activeIndex - 1 + slides.length) % slides.length;
+  const nextIndex = (activeIndex + 1) % slides.length;
+  slides[prevIndex].classList.add("side-slide");
+  slides[nextIndex].classList.add("side-slide");
+}
